3bf70c5529177ed843d9b7321fe633350466953d
Jeff Smith <toxicglados@gmail.com>
2020-10-04 22:32:44 -0500

    Inital commit

---
+++ content/test.md
@@ -0,0 +1,13 @@
+Title: Test article
+Date: 10/04/2020
+Category: Something
+
+## Hey this is an article
+
+```python
+if python_code:
+    print("this should be highlighted")
+
+```
+
+<a href=chandlerswift.com>Chandler Swift</a>
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Jeff Smith'
+SITENAME = 'Blackolive Pineapple Blog'
+SITEURL = ''
+THEME = 'theme/blackolivepineapple'
+PATH = 'content'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('GitHub', 'https://github.com/ToxicGLaDOS'),
+          ('Runescape', 'https://secure.runescape.com/m=hiscore_oldschool/hiscorepersonal?user1=Shalo123'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
